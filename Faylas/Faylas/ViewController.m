3392354002f2f3433544b63ebd50bc9a7cc99bb6
Murat POLAT <kutsalpolat@gmail.com>
2014-05-13 18:26:30 +0300

    nars

--- Faylas/Faylas/ViewController.m
+++ Faylas/Faylas/ViewController.m
@@ -17,13 +17,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-<<<<<<< HEAD
+
 	NSLog(@"Muratın amına ");
-=======
+    NSLog(@"Test12212");
 
 	
-    // Do any additional setup after loading the view, typically from a nib.
->>>>>>> c994308b0e6dcd80b6d3a9f594c85f92d2580473
+
 }
 
 - (void)didReceiveMemoryWarning
